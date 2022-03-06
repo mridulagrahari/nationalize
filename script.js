375c7a6f90fbf95e2429d64bca06d105807ffd07
mridulagrahari <57159668+mridulagrahari@users.noreply.github.com>
2022-03-06 09:38:15 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+function validateForm() {
+    let x = document.forms["form"]["name"].value;
+    if (x == "") {
+      alert("Insert a name");
+      return false;
+    }else{
+        setData();
+    }
+  }
+  
+  async function setData() {
+      // select the target element
+      const list = document.getElementById("result");
+      while (list.hasChildNodes()) {  
+          list.removeChild(list.firstChild);
+      }
+  
+      data = await getDataOfNationalize(document.forms["form"]["name"].value);
+      data.forEach(async element => {
+          country_name = await getDataOfCountry(element.country_id)
+          var new_element = document.createElement("li");
+          new_element.innerHTML = country_name;
+          document.getElementById("result").appendChild(new_element);
+          new_element.classList.add("list-group-item");  
+      });
+  }
+  
+  
+  
+  async function getDataOfNationalize(name) {
+      let url = 'https://api.nationalize.io?name=' + name;
+      try {
+          let res = await fetch(url);
+          json = await res.json();
+          return json.country;
+      } catch (error) {
+          console.log(error);
+      }
+  }
+  
+  async function getDataOfCountry(country) { 
+      let url = 'https://api.nationalize.io?name=' + country;
+      try {
+          let res = await fetch(url);
+          json = await res.json();
+          console.log(json);
+          return json.name;
+      } catch (error) {
+          console.log(error);
+      }
+  }
+
+  async function getDataOfprobability(probability) {
+    let url = 'https://api.nationalize.io?name=' + probability;
+    try {
+        let res = await fetch(url);
+        json = await res.json();
+        return json.probability;
+    } catch (error) {
+        console.log(error);
+    }
+}
